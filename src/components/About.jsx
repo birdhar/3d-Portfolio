1152974535e6250c87bbec87d5b56ab7e9d77ea7
birdhar <birat.dhar.89@gmail.com>
2023-06-18 09:15:44 -0700

    started creating portfolio with react and three.js

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+import { styles } from "../style";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const About = () => {
+  return <div>About</div>;
+};
+
+export default About;
